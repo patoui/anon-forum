d75927ff59366f79cf121c8f060c4279eb68b12e
Patrique Ouimet <patrique.ouimet@gmail.com>
2018-12-04 08:23:12 -0500

    Added checking for bad words to replies

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -1,15 +1,18 @@
+include ActivityHelper
+
 class ReplyController < ApplicationController
   before_action CheckThrottle, only: :create
 
   def create
     @post = Post.where(slug: params[:slug]).first or not_found
     @reply = Reply.new(post_id: @post.id, body: params[:body])
+
     if @reply.save
       record_activity(request, @reply)
+      redirect_to "/thread/#{@post.slug}"
     else
-      flash[:error] = "Something went wrong"
+      render template: '/post/show' and return
     end
-    redirect_to "/thread/#{@post.slug}"
   end
 
 end
