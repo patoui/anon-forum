da1d4ad86e023101c2d63fca576d9cc52ff5b43f
Patrique Ouimet <patrique.ouimet@gmail.com>
2018-12-04 18:04:51 -0500

    Updated bad word validator downcase comparison

--- app/validators/bad_word_validator.rb
+++ app/validators/bad_word_validator.rb
@@ -1,5 +1,3 @@
-include ActionView::Helpers::SanitizeHelper
-
 class BadWordValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     cache = ActiveSupport::Cache::MemoryStore.new(expires_in: 90.minutes)
@@ -7,11 +5,12 @@ class BadWordValidator < ActiveModel::EachValidator
       File.readlines('repos/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/en').map { |a| a.strip }
     end
 
-    value_array = sanitize(value.to_s).gsub(/[^0-9a-z ]/i, '').split
+    value_array = ActionController::Base.helpers.strip_tags(value.to_s).gsub(/[^0-9a-z ]/i, '').split
+    Rails.logger.debug "BAD WORDS (#{attribute}): #{value_array.to_s}"
 
     if value_array.length > 0
       for word in value_array
-        if bad_words.include? word
+        if bad_words.include? word.downcase
           record.errors[attribute] << (options[:message] || "The #{attribute} must not contain bad words.")
           break
         end
