0e03b9cfc306817762ba7fff3f4a1fbc537da4d4
Patrique Ouimet <patrique.ouimet@gmail.com>
2018-12-06 07:54:24 -0500

    Updated fetching of reply with post association

--- app/controllers/reply_upvote_controller.rb
+++ app/controllers/reply_upvote_controller.rb
@@ -5,7 +5,7 @@ class ReplyUpvoteController < ApplicationController
 
   def create
     @post = Post.find_by(slug: params[:slug]) or not_found
-    @reply = Reply.find(params[:id]) or not_found
+    @reply = @post.replies.where(id: params[:id]).first or not_found
     @reply.increment!(:upvote_count)
     record_activity('upvoted-reply', request, @reply)
     redirect_to "/thread/#{@post.slug}"
