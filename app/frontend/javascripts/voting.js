efc81d035cbe0b99db95f83f1f58c54d5bc6370a
Patrique Ouimet <patrique.ouimet@gmail.com>
2018-12-07 19:40:22 -0500

    Added AJAX voting to replies

--- app/frontend/javascripts/voting.js
+++ app/frontend/javascripts/voting.js
@@ -1,23 +1,29 @@
-let upvote = document.querySelector('.upvote');
-let upvoteUrl = upvote ? upvote.dataset.url : undefined;
-let upvoteCount = upvote ? upvote.querySelector('.vote-count') : undefined;
-let downvote = document.querySelector('.downvote');
-let downvoteUrl = downvote ? downvote.dataset.url : undefined;
-let downvoteCount = downvote ? downvote.querySelector('.vote-count') : undefined;
-console.log('file loaded');
+let upvotes = document.querySelectorAll('.upvote');
+let downvotes = document.querySelectorAll('.downvote');
 
-if (upvote && upvoteUrl && upvoteCount && downvote && downvoteUrl && downvoteCount) {
-  console.log('event listeners bound');
-  upvote.addEventListener('click', (e) => {
-    axios.post(upvoteUrl)
-      .then(() => { upvoteCount.textContent = parseInt(upvoteCount.textContent) + 1 })
-      .catch(() => { console.log('error occured') });
+if (upvotes && downvotes) {
+  upvotes.forEach((upvote) => {
+    upvote.addEventListener('click', (e) => {
+      axios.post(upvote.dataset.url)
+        .then(() => {
+          upvote
+            .querySelector('.vote-count')
+            .textContent = parseInt(upvote.querySelector('.vote-count').textContent) + 1
+        })
+        .catch(() => { console.log('error occured') });
+    });
   });
 
-  downvote.addEventListener('click', (e) => {
-    axios.post(downvoteUrl)
-      .then(() => { downvoteCount.textContent = parseInt(downvoteCount.textContent) + 1 })
-      .catch(() => { console.log('error occured') });
+  downvotes.forEach((downvote) => {
+    downvote.addEventListener('click', (e) => {
+      axios.post(downvote.dataset.url)
+        .then(() => {
+          downvote
+            .querySelector('.vote-count')
+            .textContent = parseInt(downvote.querySelector('.vote-count').textContent) + 1
+        })
+        .catch(() => { console.log('error occured') });
+    });
   });
 }
 
